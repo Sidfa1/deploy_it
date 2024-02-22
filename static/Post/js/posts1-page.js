970fb7017dc8665063fde896df30a574023a1c0e
Sidfa1 <95041318+Sidfa1@users.noreply.github.com>
2024-02-22 09:45:57 +0530

    Update posts1-page.js
    ip

--- static/Post/js/posts1-page.js
+++ static/Post/js/posts1-page.js
@@ -109,7 +109,7 @@ if (!albumDiv) {
 async function fetchData(flightNumber, source, destination, dateOfJourney) {
   try {
     const response = await fetch(
-      `http://15.206.153.73:8000/app1/postAPI/?flight_number=${flightNumber}&source=${source}&destination=${destination}&date_of_journey=${dateOfJourney}`
+      `http://3.110.29.198:8000/app1/postAPI/?flight_number=${flightNumber}&source=${source}&destination=${destination}&date_of_journey=${dateOfJourney}`
     );
  
     if (!response.ok) {
